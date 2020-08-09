f40835802e46776ce301b1c0a6a3725d1466bf94
Chloe Steffens <caforonda103@yahoo.com>
2020-08-09 22:03:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_active_students.rb
+++ db/migrate/002_active_students.rb
@@ -1,4 +1,4 @@
-class ActiveColumnStudents < ActiveRecord::Migration
+class ActiveStudents < ActiveRecord::Migration
   def change
     add_column :students, :active, :boolean, default: false
   end
